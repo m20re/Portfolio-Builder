c674632e6faa198b2ba53379c070630dc546c28d
m20re <marvinorellana@unomaha.edu>
2025-11-16 19:20:01 -0600

    Added simple homepage and changed middleware.js -> proxy.js to silence depreciation warnings.

---
+++ app/layout.js
@@ -0,0 +1,9 @@
+/* Required root layout */
+/* Can be used for navbars and the like */
+export default function RootLayout({ children }) {
+  return (
+    <html lang="en">
+      <body>{children}</body>
+    </html>
+  )
+}
---
+++ app/page.js
@@ -0,0 +1,4 @@
+/* Sample page to test configuration */
+export default function Page() {
+  return <h1>Hello, Next.js!</h1>
+}
