98c6d93eaf7aff1a37cb04781ff1f36f1ee24c0d
m20re <marvinorellana@unomaha.edu>
2025-12-11 00:38:06 -0600

    Attempts to remidiate env issues within amplify

---
+++ next.config.js
@@ -0,0 +1,11 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  env: {
+    DATABASE_URL: process.env.DATABASE_URL,
+    JWT_SECRET: process.env.JWT_SECRET,
+    NEXTAUTH_SECRET: process.env.NEXTAUTH_SECRET,
+    NEXTAUTH_URL: process.env.NEXTAUTH_URL,
+  },
+};
+
+module.exports = nextConfig;
